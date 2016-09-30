3b543cd5dd8d80e721fb8e7a8b7f519d9d6c7792
humengting <1130558141@qq.com>
2016-09-30 17:01:18 +0800

    实现轮播图封装函数

--- www/src/js/index.js
+++ www/src/js/index.js
@@ -1,30 +1,71 @@
 /**
  * Created by my on 2016/9/30.
  */
+
 //  console.log()
-var lisIndex = 0;
-$("#navMiddleSelect li").bind("click",function(){
-    //li 的下标   数值型
-    console.log($(this).index());
-    lisIndex =  $(this).index();
-    if(lisIndex==-1){
-        lisIndex =0;
+
+
+// var lisIndex = 0;
+// var phoIndex = 0;
+// var timer;
+// 单个的轮播图
+// $("#navMiddleSelect li").bind("click",function(){
+//     //li 的下标   数值型
+//     //console.log("li 的下标--- "+$(this).index());
+//     lisIndex =  $(this).index();
+//     if(lisIndex==-1){
+//         lisIndex =0;
+//     }
+//     $("#navMiddle img").get(0).src = "../images/headnav"+ (lisIndex+1)+".jpg";
+//     $("#navMiddleSelect").children().removeClass("selectActive");
+//     $(this).addClass("selectActive");
+//     imgScroll();
+// });
+// imgScroll();
+// function imgScroll(){
+//     clearInterval(timer);
+//     timer=setInterval(function(){
+//         if(lisIndex == 7){
+//             lisIndex = -1 ;
+//         }
+//         //console.log("lisIndex----"+lisIndex);
+//         lisIndex++;
+//         $("#navMiddle img").get(0).src = "../images/headnav"+ (lisIndex+1)+".jpg";
+//         $("#navMiddleSelect").children().removeClass("selectActive");
+//         $("#navMiddleSelect li").eq(lisIndex).addClass("selectActive");
+//         //console.log("++lisIndex++++++"+lisIndex);
+//     },5000);
+// }
+adScroll("#navMiddleSelect","#navMiddle","headnav","selectActive",7,5000)
+adScroll("#middleNav","#navButtom","adScroll","first",2,3000);
+adScroll("#adScroll","#floorlfet-buttom","flool1scor","floorfirstli",2,3000);
+
+
+//封装函数  点父亲，图片父亲,图片名称，激活的Class ，图片最大数，最小数
+function adScroll(objUl,objImg,objImgName,activeClass,_max,_time){
+   //一定要先清空phoIndex；这样才不会重复
+    var phoIndex = 0;
+    $(objUl+" li").bind("click", function () {
+        clearInterval(objUl.timer);
+        phoIndex =  $(this).index();
+        if(phoIndex==-1){
+            phoIndex =0;
+        }
+        $(objImg+" img").get(0).src = "../images/" + objImgName+ (phoIndex+1)+".jpg";
+        $(objUl).children().removeClass(activeClass);
+        $(this).addClass(activeClass);
+    })
+    phoScroll();
+    function phoScroll(){
+        clearInterval(objUl.timer);
+        objUl.timer=setInterval(function(){
+            if(phoIndex == _max){
+                phoIndex = -1 ;
+            }
+            phoIndex++;
+            $(objImg+" img").get(0).src = "../images/" + objImgName+ (phoIndex+1)+".jpg";
+            $(objUl).children().removeClass(activeClass);
+            $(objUl+" li").eq(phoIndex).addClass(activeClass);
+        },_time);
     }
-    $("#navMiddle img").get(0).src = "../images/headnav"+ (lisIndex+1)+".jpg";
-    $("#navMiddleSelect").children().removeClass("selectActive");
-    $(this).addClass("selectActive");
-})
-var headImgIndex = 0;
-timer=setInterval(function(){
-    if(lisIndex == 7){
-        lisIndex = -1 ;
-    }
-    console.log(lisIndex)
-    lisIndex++;
-    $("#navMiddle img").get(0).src = "../images/headnav"+ (lisIndex+1)+".jpg";
-
-    $("#navMiddleSelect").children().removeClass("selectActive");
-    $("#navMiddleSelect li").eq(lisIndex).addClass("selectActive");
-
-
-},3000)
+}
