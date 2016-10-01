7447810e8db475cac44c1a6e3bc3f47a45a09774
humengting <1130558141@qq.com>
2016-10-01 22:58:34 +0800

    首页大部分功能实现  但是还有很多没弄完  东西太多了

--- www/src/js/index.js
+++ www/src/js/index.js
@@ -2,48 +2,16 @@
  * Created by my on 2016/9/30.
  */
 
-//  console.log()
-
-
-// var lisIndex = 0;
-// var phoIndex = 0;
-// var timer;
-// 单个的轮播图
-// $("#navMiddleSelect li").bind("click",function(){
-//     //li 的下标   数值型
-//     //console.log("li 的下标--- "+$(this).index());
-//     lisIndex =  $(this).index();
-//     if(lisIndex==-1){
-//         lisIndex =0;
-//     }
-//     $("#navMiddle img").get(0).src = "../images/headnav"+ (lisIndex+1)+".jpg";
-//     $("#navMiddleSelect").children().removeClass("selectActive");
-//     $(this).addClass("selectActive");
-//     imgScroll();
-// });
-// imgScroll();
-// function imgScroll(){
-//     clearInterval(timer);
-//     timer=setInterval(function(){
-//         if(lisIndex == 7){
-//             lisIndex = -1 ;
-//         }
-//         //console.log("lisIndex----"+lisIndex);
-//         lisIndex++;
-//         $("#navMiddle img").get(0).src = "../images/headnav"+ (lisIndex+1)+".jpg";
-//         $("#navMiddleSelect").children().removeClass("selectActive");
-//         $("#navMiddleSelect li").eq(lisIndex).addClass("selectActive");
-//         //console.log("++lisIndex++++++"+lisIndex);
-//     },5000);
-// }
-
-
-//轮播图  调用函数
+//轮播图
 adScroll("#navMiddleSelect","#navMiddle","headnav","selectActive",7,5000,true)
 adScroll("#middleNav","#navButtom","adScroll","first",2,3000,true);
 adScroll("#adScroll","#floorlfet-buttom","flool1scor","floorfirstli",2,3000,true);
 adScroll(".floorMiddleUl",".floorMiddle","floorMiddle","floorfirstli",2,3000,false);
-
+//无缝滚动
+marqueeScroll("#nav-right","#navMarquee","marqueeR",1012,3000);
+marqueeScroll("#main-top-scroll","#mainMarqueeScoll","mainMarqueeR",796,5000);
+//面板切换
+planChange();
 
 //封装函数  点父亲，图片父亲,图片名称，激活的Class ，图片最大数，最小数，是否需要自动播放
 function adScroll(objUl,objImg,objImgName,activeClass,_max,_time,isFlage){
@@ -62,7 +30,6 @@ function adScroll(objUl,objImg,objImgName,activeClass,_max,_time,isFlage){
     })
     //自动播放
     if(isFlage){
-
         phoScroll();
         function phoScroll(){
             clearInterval(objUl.timer);
@@ -77,69 +44,14 @@ function adScroll(objUl,objImg,objImgName,activeClass,_max,_time,isFlage){
             },_time);
         }
     }
-
 }
-//  导航栏滚动
-//var pageIndex=0;
-////console.log($("#navMarquee").children())
-//$("#nav-right li").bind("click",function(){
-//    clearInterval(Marqueetimer);
-//    if($(this).hasClass("marqueeR")){
-//        pageIndex++;
-//        if(pageIndex==3){
-//            pageIndex=0;
-//        }
-//    }else{
-//        pageIndex--;
-//        if(pageIndex==-1)
-//            pageIndex=2;
-//    }
-//    console.log(pageIndex)
-//    $("#navMarquee").stop().animate( { left:-pageIndex*1012} ,"slow");
-//});
-//
-//Marqueetimer=setInterval( function(){
-//    pageIndex++;
-//    if(pageIndex==3){
-//        pageIndex=0;
-//    }
-//    $("#navMarquee").stop().animate( { left:-pageIndex*1012} ,"slow");
-//},4000);
-////中间加倒计时滚
-//var pagesIndex=0;
-////console.log($("#navMarquee").children())
-//$("#main-top-scroll li").bind("click",function(){
-//    clearInterval(mainMarqueetimer);
-//    if($(this).hasClass("mainMarqueeR")){
-//        pagesIndex++;
-//        if(pagesIndex==3){
-//            pagesIndex=0;
-//        }
-//    }else{
-//        pagesIndex--;
-//        if(pagesIndex==-1)
-//            pagesIndex=2;
-//    }
-//    console.log(pagesIndex)
-//    $("#mainMarqueeScoll").stop().animate( { left:-pagesIndex*796} ,"slow");
-//});
-//
-//mainMarqueetimer=setInterval( function(){
-//    pagesIndex++;
-//    if(pagesIndex==3){
-//        pagesIndex=0;
-//    }
-//    $("#mainMarqueeScoll").stop().animate( { left:-pagesIndex*796} ,"slow");
-//},5000);
-var pagesIndex=0;
-marqueeScroll("#nav-right","#navMarquee","marqueeR",1012,3000);
-marqueeScroll("#main-top-scroll","#mainMarqueeScoll","mainMarqueeR",796,5000);
 //objUL 指的是左右按钮 的父元素    objImg指的是要移动的元素
 // _classR指的是向右的按钮  _length指的是长度要转一页的长度
 function marqueeScroll( objUL,objImg, _classR,_length,_time){
+    var pagesIndex=0;
     $(objUL+" li").css("cursor","pointer");
     $(objUL+" li").bind("click",function(){
-        clearInterval(objULtimer);
+        clearInterval(objUL.times);
         if($(this).hasClass(_classR)) {
             pagesIndex++;
             console.log(pagesIndex);
@@ -155,39 +67,43 @@ function marqueeScroll( objUL,objImg, _classR,_length,_time){
                 }
         }
         $(objImg).stop().animate( { left: parseInt(-pagesIndex*_length)},"slow" );
-
     });
-    objULtimer=setInterval( function(){
-        pagesIndex++;
-        if(pagesIndex==3){
-            pagesIndex=0;
-        }
-        $(objImg).stop().animate( { left: parseInt(-pagesIndex*_length)},"slow" );
-    },_time);
+    pageScroll();
+    function pageScroll(){
+        clearInterval(objUL.times);
+        objUL.times=setInterval( function(){
+            pagesIndex++;
+            if(pagesIndex==3){
+                pagesIndex=0;
+            }
+            $(objImg).stop().animate( { left: -pagesIndex*_length},"slow" );
+        },_time);
+    }
 }
-var arr1 = ["福临门金粳稻5kg","长生特香花生油","2015中秋猪蹄","青岛啤酒经典礼盒装","波尼亚福运来礼盒","福记福月月饼礼盒640g"]
-var prise1 = ["34.50","129.80","180.00","70.00","119.00","58.00"]
-var prise2 = ["21.50","19.50","49.90","21.90","2.80","32.00"]
-var prise3 = ["34.50","70.00","21.90","119.00","34.50","21.90"]
-var prise4 = ["70.00","34.50","21.90","119.00","34.50","70.00"]
-var prise5 = ["70.00","21.90","119.00","34.50","21.90","34.50"]
-var arr2 = ["利麦特一粉5kg","欣和味达美1.3L","长生特香花生油1.8L","中盐低钠海藻盐400g",
-    "【南纬三十七度】澳大利亚进口"];
-var arr5 = ["利麦特一粉5kg","欣和味达美1.3L","长生特香花生油1.8L","中盐低钠海藻盐400g",
-    "【南纬三十七度】澳大利亚进口"];
-var arr3 = ["青食六联包钙奶饼干225g*6","乐事薯片无限104克三罐装组合包","有友泡凤爪山椒味180g",
-    "青岛啤酒红冰醇10度330ml","德芙榛仁葡萄干巧克力碗装243g","久久丫微辣鸭脖118g"];
-var arr4 = ["清风原木纯品3层240段卷筒纸","心相印优选1800g三层卷纸BT2310",
-    "维达店庆装1800克三层无芯卷纸","顺清柔6连包抽纸","珍爱洁面湿巾B 10片装","清风漫威4层10包手帕纸B66ATTZV"]
-var arr = [arr1,arr2,arr3,arr4,arr5];
-var prise = [prise1,prise2,prise3,prise4,prise5];
-var plans = ["plan1","plan2","plan3","plan4","plan5"]
-//console.log(arr[1][0])
 //面板切换
-planChange();
 function planChange(){
+    //定义内容
+    var arr1 = ["福临门金粳稻5kg","长生特香花生油","2015中秋猪蹄","青岛啤酒经典礼盒装","波尼亚福运来礼盒","福记福月月饼礼盒640g"]
+    var prise1 = ["34.50","129.80","180.00","70.00","119.00","58.00"]
+    var prise2 = ["21.50","19.50","49.90","21.90","2.80","32.00"]
+    var prise3 = ["34.50","70.00","21.90","119.00","34.50","21.90"]
+    var prise4 = ["70.00","34.50","21.90","119.00","34.50","70.00"]
+    var prise5 = ["70.00","21.90","119.00","34.50","21.90","34.50"]
+    var arr2 = ["利麦特一粉5kg","欣和味达美1.3L","长生特香花生油1.8L","中盐低钠海藻盐400g",
+        "【南纬三十七度】澳大利亚进口"];
+    var arr5 = ["利麦特一粉5kg","欣和味达美1.3L","长生特香花生油1.8L","中盐低钠海藻盐400g",
+        "【南纬三十七度】澳大利亚进口"];
+    var arr3 = ["青食六联包钙奶饼干225g*6","乐事薯片无限104克三罐装组合包","有友泡凤爪山椒味180g",
+        "青岛啤酒红冰醇10度330ml","德芙榛仁葡萄干巧克力碗装243g","久久丫微辣鸭脖118g"];
+    var arr4 = ["清风原木纯品3层240段卷筒纸","心相印优选1800g三层卷纸BT2310",
+        "维达店庆装1800克三层无芯卷纸","顺清柔6连包抽纸","珍爱洁面湿巾B 10片装","清风漫威4层10包手帕纸B66ATTZV"]
+    var arr = [arr1,arr2,arr3,arr4,arr5];
+    var prise = [prise1,prise2,prise3,prise4,prise5];
+    var plans = ["plan1","plan2","plan3","plan4","plan5"];
+//console.log(arr[1][0])
     $("#floorNavtop li").bind("click mouseover",function(){
         //$("#floorNavtop").children().removeClass("lihover");
+        $(this).css("cursor","pointer");
         var liIndex = $(this).index();
         console.log(liIndex)
         $(this).siblings().removeClass("lihover");
@@ -205,3 +121,59 @@ function planChange(){
         })
     });
 }
+//倒计时
+function countDownTime(){
+    //截止时间
+    var endTime = new Date("2016/10/4 12:00");
+    //var d = new Date();
+    //d.setDate(d.getDate()+3);
+    //console.log(d);
+    //起始时间
+    var nowTime = new Date();
+    //时间差
+    var _time = endTime.getTime() - nowTime.getTime();
+    //console.log( _time)
+    var d = Math.floor(_time/1000/60/60/24);
+    var h = Math.floor(_time/1000/60/60%24);
+    var m = Math.floor(_time/1000/60%60);
+    var s = Math.floor(_time/1000%60);
+    //console.log(d.length);
+    //d = settype(d);
+    //settype(h);
+    //settype(m);
+    //settype(s);
+    //function settype(num){
+    //    num.toString();
+    //    if(num.length>1){
+    //        console.log(num.length);
+    //        num = "0"+num;
+    //        return num;
+    //    }
+    //}
+    //console.log(d);
+    //console.log(d+"天"+h+":"+m+":"+s);
+    return d+"天"+h+":"+m+":"+s;
+
+}
+countDown();
+function countDown(){
+    //console.log("jjj ")
+    Downtimer=setInterval( function(){
+        $("#mainMarqueeScoll dl p span").html(countDownTime())
+    },10)
+
+}
+
+$("#leftnav").bind( "click mouseenter",function(){
+    $("#floatNav").css("display","block");
+})
+$("#floatNav").bind( "mouseenter",function(){
+    $("#floatNav").css("display","block");
+})
+$("#leftnav").bind( "mouseleave",function(){
+    $("#floatNav").css("display","none");
+})
+$("#floatNav").bind( "mouseleave",function(){
+    $("#floatNav").css("display","none");
+})
+
